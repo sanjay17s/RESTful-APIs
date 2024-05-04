8c9a41ccd687884ddff3b525cf23142690811244
sanjay17s <sanjay.17suresh2000@gmail.com>
2024-05-04 15:41:29 +0530

    added notes

--- src/main/java/com/example/RESTful/APIs/GreetingController.java
+++ src/main/java/com/example/RESTful/APIs/GreetingController.java
@@ -11,5 +11,12 @@ public class GreetingController {
         return new Greeting(1, "Hello, " + name + "!");
     }
 
+    /*The client (web browser) sends an HTTP request to the Tomcat server.
+    Tomcat, acting as the embedded servlet container, receives the request and forwards it to the Spring MVC Dispatcher.
+    The Dispatcher maps the request to the appropriate Controller based on the request URL.
+    The Controller processes the request, interacts with the Model (if necessary), and returns a response.
+    If needed, the Controller uses the Model to retrieve or manipulate data.
+    Finally, the response is sent back to the client (web browser) via Tomcat.*/
+
 }
 
